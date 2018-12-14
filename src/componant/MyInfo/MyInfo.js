f24f2dd77754f498d37c3cb33b3675e6f8fd6c30
AhmedEldessouki <44158955+AhmedEldessouki@users.noreply.github.com>
2018-12-14 17:22:45 +0300

    Css

--- src/componant/MyInfo/MyInfo.js
+++ src/componant/MyInfo/MyInfo.js
@@ -26,7 +26,7 @@ export default class MyInfo extends Component {
     console.log(profile)
     return (
       <div className="MyInfo">
-        <div>
+        <div className="first-container">
           <div className="img-container">
             <img src={profile.avatar_url} alt="profilePicture" />
           </div>
@@ -42,9 +42,13 @@ export default class MyInfo extends Component {
             </li>
           </ul>
         </div>
-        <div className="about-me">
+        <div className="second-container">
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
+          Welcome I'm Ahmed Eldessouki I work as <em>Fron-End Developer</em> I'm from Egypt 
+          and I'm currently living in Turkey. I graduated from Universty Of South Wales and 
+          right now I work in RoomMe as an entry level Front-End Developer. I'm very passionate 
+          newbie Front-End Developer who loves  to develop himself and stay up-to-date with the 
+          new <em>technologies and technics</em>.
           </p>
         </div>
       </div>
