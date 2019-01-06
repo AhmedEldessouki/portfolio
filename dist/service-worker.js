c51bbec259f6af3caea2252dd074d735c38c9ed3
AhmedEldessouki <44158955+AhmedEldessouki@users.noreply.github.com>
2019-01-06 03:15:48 +0300

    fixxed

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5cd80b4ad2dc267550243fbdbdbba633.js"
+  "/precache-manifest.a1f623ccfb91c625ed8edfbb86595cca.js"
 );
 
 workbox.clientsClaim();
