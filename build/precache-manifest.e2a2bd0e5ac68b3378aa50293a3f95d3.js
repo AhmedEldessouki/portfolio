eea1fe934335a335b246b95780e517e8db0c5d14
Nemo <Nemo@NEMO>
2020-07-06 17:21:01 +0300

    build folder

---
+++ build/precache-manifest.e2a2bd0e5ac68b3378aa50293a3f95d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae5fc6d5e2b86e746f8d",
+    "url": "/static/css/main.ef0f4a3e.chunk.css"
+  },
+  {
+    "revision": "ae5fc6d5e2b86e746f8d",
+    "url": "/static/js/main.ae5fc6d5.chunk.js"
+  },
+  {
+    "revision": "552a0bf29384763b2c58",
+    "url": "/static/css/1.88dd65dd.chunk.css"
+  },
+  {
+    "revision": "552a0bf29384763b2c58",
+    "url": "/static/js/1.552a0bf2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0e512c7ee222713e46e764784c7bc23",
+    "url": "/static/media/react.f0e512c7.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "c27052ec5553f89884da81832d44b971",
+    "url": "/index.html"
+  }
+];
