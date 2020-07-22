71e3f644da5a24b5a4493a947159350bcaccf252
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-07-22 15:34:26 +0300

    build and bootstrap upgrade

---
+++ dist/precache-manifest.a8aac13ef33542353654fc313def3feb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d1bf7775ac27e97e2e98b8764c816f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9e52986c4fa9a8b3757",
+    "url": "/static/css/2.767dc2b6.chunk.css"
+  },
+  {
+    "revision": "5e0242779c3593bfbcfc",
+    "url": "/static/css/main.7c866879.chunk.css"
+  },
+  {
+    "revision": "f9e52986c4fa9a8b3757",
+    "url": "/static/js/2.2b04fbde.chunk.js"
+  },
+  {
+    "revision": "69ad1f2fc14410b256e80f4f94f38676",
+    "url": "/static/js/2.2b04fbde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0242779c3593bfbcfc",
+    "url": "/static/js/main.950489a0.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "26ef1cbcfa05495818a9d63927b452cb",
+    "url": "/static/media/react.26ef1cbc.svg"
+  }
+]);
