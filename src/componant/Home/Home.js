315b7335ceededde3a3516eb41d2323476a1110d
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-07-25 00:52:07 +0300

    when you release that you were being mentored in a wrong way, and later check your code and be like WTF was that

--- src/componant/Home/Home.js
+++ src/componant/Home/Home.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
-import './Home.scss';
+import React, { Component } from 'react'
+import './Home.scss'
 import MyInfo from './MyInfo/MyInfo'
 import Projects from './Projects/Projects'
 import ContactMe from './ContactMe/ContactMe'
 import MyFooter from './MyFooter/MyFooter'
-import ScrollUpButton from "react-scroll-up-button";
-import {connect} from 'react-redux'
+import ScrollUpButton from 'react-scroll-up-button'
+import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
-import {compose} from 'redux'
+import { compose } from 'redux'
 import AuthNavlinks from '../Navigation/AuthNavlinks'
+import UnAuthNavlinks from '../Navigation/UnAuthNavlinks'
+// import my from '../../assets/my.svg'
 
 class Home extends Component {
-  constructor (){
-    super ();
+  constructor() {
+    super()
     this.state = {
       intervalId: 0,
-      title: 'Nemo Adam'
+      title: 'Nemo Adam',
     }
   }
   render() {
-    const {projectsData, auth, profile} = this.props;
-    const links = auth.uid ?
-      <AuthNavlinks profile={profile} title={this.state.title}/>
-      :(
-        <div className="myNav-container">
-          <div className="my-name">
-            <span>Nemo Adam</span>
-          </div>
-          <div className="scroll-spy">
-            <a className="scroll-spy-item" href="/">Home</a>
-            <a className="scroll-spy-item" href={"#projects"}>Projects</a>
-            <a className="scroll-spy-item" href="#contactMe">Contact Me</a>
-          </div>
-        </div>
-      )
+    const { projectsData, auth, profile } = this.props
+    const links = auth.uid ? (
+      <AuthNavlinks profile={profile} title={this.state.title} />
+    ) : (
+      <UnAuthNavlinks />
+    )
+
     return (
       <div className="Home">
         <header className="Home-header" id="1">
@@ -43,34 +37,34 @@ class Home extends Component {
         <ScrollUpButton
           StopPosition={0}
           ShowAtPosition={150}
-          EasingType='easeOutCubic'
+          EasingType="easeOutCubic"
           AnimationDuration={500}
-          ContainerClassName='ScrollUpButton__Container'
-          TransitionClassName='ScrollUpButton__Toggled'
+          ContainerClassName="ScrollUpButton__Container"
+          TransitionClassName="ScrollUpButton__Toggled"
         />
         <main id="projects">
-          <Projects projectsData={projectsData}/>
+          <Projects projectsData={projectsData} />
         </main>
         <footer id="contactMe">
           <ContactMe />
           <MyFooter />
         </footer>
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = (state) => {
-  return{
+  return {
     projectsData: state.firestore.ordered.projects,
-    auth:state.firebase.auth,
+    auth: state.firebase.auth,
     profile: state.firebase.profile,
   }
-};
+}
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    {collection: 'projects', orderBy: ['createdAt', 'desc']},
-    {collection:'projectLogo'}
+    { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+    { collection: 'projectLogo' },
   ])
 )(Home)
--- src/componant/Navigation/AuthNavlinks.js
+++ src/componant/Navigation/AuthNavlinks.js
@@ -1,56 +1,58 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {signOut} from '../../Store/Actions/AuthActions'
+import { signOut } from '../../Store/Actions/AuthActions'
 import './Navigation.scss'
-import {Navbar, Nav, NavItem} from 'react-bootstrap'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import my from '../../assets/my.svg'
+// import AhmedEldessouki from '../../assets/AhmedEldessouki.svg'
 
 const AuthNavlinks = (props) => {
-  return(
-    <div>
-      <div>
-        <Navbar className="Navigation" inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <span>{props.title}</span>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem eventKey={1} href={"/"}>
-                Home
-              </NavItem>
-              <NavItem eventKey={2} href={"/#projects"}>
-                Projects
-              </NavItem>
-              <NavItem eventKey={3} href={"/#contactMe"}>
-                Contact Me
-              </NavItem>
-              <NavItem eventKey={4} href={"/dashboard"}>
-                DashBoard
-              </NavItem>
-              <NavItem eventKey={5} href={"/create-project"}>
-                Create Project
-              </NavItem>
-              <NavItem eventKey={5} href={"/signup"}>
-                SignUp
-              </NavItem>
-              <NavItem eventKey={5} onClick={props.signOut} href={"/"}>
-                SignOut
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    </div>
+  return (
+    <Navbar className="Navigation" inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          {/* <span>{props.title}</span> */}
+          <img
+            src={my}
+            alt={'Ahmed ElDessouki'}
+            style={{ width: '150px', padding: '0' }}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem eventKey={1} href={'/'}>
+            Home
+          </NavItem>
+          <NavItem eventKey={2} href={'/#projects'}>
+            Projects
+          </NavItem>
+          <NavItem eventKey={3} href={'/#contactMe'}>
+            Contact Me
+          </NavItem>
+          <NavItem eventKey={4} href={'/dashboard'}>
+            DashBoard
+          </NavItem>
+          <NavItem eventKey={5} href={'/create-project'}>
+            Create Project
+          </NavItem>
+          <NavItem eventKey={5} href={'/signup'}>
+            SignUp
+          </NavItem>
+          <NavItem eventKey={5} onClick={props.signOut} href={'/'}>
+            SignOut
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
-};
-
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    signOut: ()=> dispatch(signOut())
+    signOut: () => dispatch(signOut()),
   }
-};
+}
 
 export default connect(null, mapDispatchToProps)(AuthNavlinks)
