8654d6d218de911653b4444e11f2d04c040a3370
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-07-28 12:07:33 +0300

    app has been deployed on firebase.. functions didnt need to check and activate googlecloud

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-let firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+let firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
 
 admin.initializeApp(firebaseConfig)
 
@@ -11,56 +11,53 @@ admin.initializeApp(firebaseConfig)
 //  response.send("Hello from Firebase!");
 // });
 
-const createNotification = (notification => {
-  return admin.firestore().collection('notifications')
-  .add(notification)
-  .then(doc => console.log('notification added', doc));
-})
+const createNotification = (notification) => {
+  return admin
+    .firestore()
+    .collection('notifications')
+    .add(notification)
+    .then((doc) => console.log('notification added', doc))
+}
 
 exports.projectCreated = functions.firestore
-.document('projects/{projectId}')
-.onCreate( doc =>{
-
-  const project = doc.data();
-  const notification = {
-    content: 'Added a new project',
-    user: `${project.authorFirstName} ${project.authorLastName}`,
-    time: admin.firestore.FieldValue.serverTimestamp()
-  }
-
-  return createNotification(notification)
-
-})
-
-exports.userJoined = functions.auth.user()
-.onCreate(user => {
-
-  return admin.firestore().collection('users')
-  .doc(user.uid).get().then( doc => {
-
-    const newUser = doc.data()
+  .document('projects/{projectId}')
+  .onCreate((doc) => {
+    const project = doc.data()
     const notification = {
-      content: 'Joined The App',
-      user: `${newUser.firstName} ${newUser.lastName}`,
-      time: admin.firestore.FieldValue.serverTimestamp()
+      content: 'Added a new project',
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp(),
     }
-    return createNotification(notification)
 
+    return createNotification(notification)
   })
 
+exports.userJoined = functions.auth.user().onCreate((user) => {
+  return admin
+    .firestore()
+    .collection('users')
+    .doc(user.uid)
+    .get()
+    .then((doc) => {
+      const newUser = doc.data()
+      const notification = {
+        content: 'Joined The App',
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp(),
+      }
+      return createNotification(notification)
+    })
 })
 
 exports.messageReceived = functions.firestore
-.document('contactedMe/{contactId}')
-.onCreate( doc =>{
+  .document('contactedMe/{contactId}')
+  .onCreate((doc) => {
+    const contact = doc.data()
+    const notification = {
+      content: 'message received',
+      user: `${contact.contactName}`,
+      time: admin.firestore.FieldValue.serverTimestamp(),
+    }
 
-  const contact = doc.data();
-  const notification = {
-    content: 'message received',
-    user: `${contact.contactName}`,
-    time: admin.firestore.FieldValue.serverTimestamp()
-  }
-
-  return createNotification(notification)
-
-})
+    return createNotification(notification)
+  })
