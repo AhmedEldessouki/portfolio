90d4ffff2de92db4140838610067a8c8761967be
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-07-29 19:08:16 +0300

    added lazy load to routes ... no apperent change in preformance

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { lazy, Suspense } from 'react'
 import './App.scss'
-import Home from './componant/Home/Home'
 import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom'
-import Dashboard from './componant/Dashboard/Dashboard'
-import MessageDetails from './componant/Dashboard/Messaging/MessageDetails'
-import SignIn from './componant/Auth/SignIn'
-import SignUp from './componant/Auth/SignUp'
-import ProjectDetails from './componant/Home/Projects/ProjectDetails'
-import CreateProject from './componant/Home/Projects/CreateProject'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
-  useEffect(() => {
-    document.title = 'Ahmed Eldessouki'
-  })
+const Home = React.lazy(() => import('./componant/Home/Home'))
+const ProjectDetails = lazy(() =>
+  import('./componant/Home/Projects/ProjectDetails')
+)
+const SignIn = lazy(() => import('./componant/Auth/SignIn'))
 
+const Dashboard = lazy(() => import('./componant/Dashboard/Dashboard'))
+const MessageDetails = lazy(() =>
+  import('./componant/Dashboard/Messaging/MessageDetails')
+)
+const SignUp = lazy(() => import('./componant/Auth/SignUp'))
+const CreateProject = lazy(() =>
+  import('./componant/Home/Projects/CreateProject')
+)
+
+function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/projects/:id" component={ProjectDetails} />
-          <Route path="/messages/:id" component={MessageDetails} />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/SignUp" component={SignUp} />
-          <Route path="/create-project" component={CreateProject} />
-          <Route path="/edit/:id" component={CreateProject} />
-          <Redirect from="*" to="/" />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/projects/:id" component={ProjectDetails} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/messages/:id" component={MessageDetails} />
+            <Route path="/SignUp" component={SignUp} />
+            <Route path="/create-project" component={CreateProject} />
+            <Route path="/edit/:id" component={CreateProject} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
       <ToastContainer autoClose={2000} />
     </div>
