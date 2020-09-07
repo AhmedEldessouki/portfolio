b7c9e1f17a9ef521c979a3b62c5c2b404664bb86
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-09-07 16:59:42 +0300

    pop-up component done

--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -1,71 +1,134 @@
-import './PopUp.scss'
-import React, {Component} from 'react';
-import {GoTrashcan} from "react-icons/go";
-import {deleteProject} from "../../Store/Actions/ProjectsActions";
-import {deleteMessage} from "../../Store/Actions/ContactedMeActions";
-import { deleteNotification} from '../../Store/Actions/NotificationActions';
-import {connect} from "react-redux";
+/**@jsx jsx */
+import { jsx, css } from '@emotion/core'
+import { useState, Fragment } from 'react'
+import { GoTrashcan } from 'react-icons/go'
+import { deleteProject } from '../../Store/Actions/ProjectsActions'
+import { deleteMessage } from '../../Store/Actions/ContactedMeActions'
+import { deleteNotification } from '../../Store/Actions/NotificationActions'
+import { connect } from 'react-redux'
 
-class PopUp extends Component {
-  constructor() {
-    super();
-    this.state = {
-      show: false
-    };
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.handleDelete= this.handleDelete.bind(this);
-  }
+import { colors, weights } from '../../Styles'
 
-  handleClose() {
-    this.setState({ show: false });
-  }
-  handleDelete() {
-    if (this.props.project){
-      this.props.deleteProject(this.props.project)
-    } else if (this.props.contact){
-      this.props.deleteMessage(this.props.contact)
-    }else if (this.props.notification){
-      this.props.deleteNotification(this.props.notification)
+function PopUp({ project, deleteProject, contact, deleteMessage, title }) {
+  const popWrapper = css`
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
     }
-    this.setState({ show: false });
+
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    position: absolute;
+    top: calc(50% - calc(288.3px / 2));
+    left: calc(50% - calc(554.8px / 2));
+    background-color: ${colors.independenceBlue};
+    opacity: 94.7%;
+    border: 10px solid ${colors.darkBlue};
+    padding: 3% 7%;
+    border-radius: 29%;
+    width: 323px;
+    height: 178px;
+    justify-content: center;
+    align-items: center;
+    animation: fadeIn 0.5s ease-in-out;
+    box-shadow: 0 0 140px 100px ${colors.kindaDarkBlue};
+  `
+  const btn = css`
+    :before {
+      width: 100%;
+    }
+    background-color: ${colors.whiteFaded};
+    color: ${colors.independenceBlue};
+    border-radius: 14%;
+    border: 0px;
+    font-size: 140%;
+    padding: 4px 36px;
+    margin: 0 15px;
+    :hover,
+    :focus {
+      color: ${colors.aliceLightBlue};
+      background-color: ${colors.darkBlue};
+    }
+  `
+  const btnTrash = css`
+    background: inherit;
+    box-shadow: none;
+    border: 0;
+    margin: 0;
+    padding: 0;
+    color: ${colors.aliceLightBlue};
+    font-size: 2rem;
+    font-weight: ${weights.black};
+    :hover,
+    :focus {
+      color: ${colors.burgundyRed};
+    }
+  `
+  const [show, setShow] = useState(true)
+
+  function handleDelete() {
+    if (project) {
+      deleteProject(project)
+      setShow(!show)
+    } else if (contact) {
+      deleteMessage(contact)
+      setShow(!show)
+    } else {
+      console.log('damnnnnnn i worked')
+      setShow({ show: false })
+    }
   }
-
-  handleShow() {
-    this.setState({ show: true });
-  }
-
-  render () {
-    const HIDE = this.props.project || this.props.contact || this.props.notification ? {display:""}: {display:'none'}
-    const content =this.props.title;
-
-    return (
-      <div className="PopUp" style={{display: "none"}}>
-        <button className="toggle-button primary" onClick={this.handleShow}>
-          <GoTrashcan />
-        </button>
-        <div className="static-modal" >
-            <header>
-              <h1>Warning</h1>
-            </header>
-            <h2>Do you want to delete this {content}</h2>
-            <footer>
-              <button onClick={this.handleClose}>No</button>
-              <button className="danger"  style={{HIDE}} onClick={this.handleDelete}>
-                Yes
-              </button>
-            </footer>
+  return (
+    <Fragment>
+      <button
+        css={btnTrash}
+        onClick={() => [setShow(!show), console.log('clicked')]}
+      >
+        <GoTrashcan />
+      </button>
+      {show ? (
+        console.log('bitch')
+      ) : (
+        <div css={popWrapper} id='popup'>
+          <header>
+            <h1>Warning</h1>
+          </header>
+          <h2>Do you want to delete this {title}</h2>
+          <footer>
+            <button css={btn} onClick={() => setShow(!show)}>
+              Nah!
+            </button>
+            <button
+              css={[
+                btn,
+                css`
+                  :hover {
+                    background-color: ${colors.burgundyRed};
+                  }
+                `,
+              ]}
+              onClick={handleDelete}
+            >
+              Yup!
+            </button>
+          </footer>
         </div>
-      </div>
-    );
-  }
+      )}
+    </Fragment>
+  )
 }
 
-const mapDispatchToProps = (dispatch) =>{
+const mapDispatchToProps = dispatch => {
   return {
-    deleteProject: (project) => dispatch(deleteProject(project)),
-    deleteMessage: (contact) => dispatch(deleteMessage(contact)),
-    deleteNotification: (notification) => dispatch(deleteNotification(notification)),
+    deleteProject: project => dispatch(deleteProject(project)),
+    deleteMessage: contact => dispatch(deleteMessage(contact)),
+    deleteNotification: notification =>
+      dispatch(deleteNotification(notification)),
   }
-};
-export default connect(null,mapDispatchToProps)(PopUp)
+}
+export default connect(null, mapDispatchToProps)(PopUp)
