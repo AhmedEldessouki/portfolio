7f6e6877659e731708103f2a8247aed2006f107c
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-09-11 22:49:39 +0300

    ProjectSummary Component done

--- src/components/Home/Projects/ProjectsSummary.js
+++ src/components/Home/Projects/ProjectsSummary.js
@@ -1,37 +1,59 @@
-import React from 'react'
-import './Styles/ProjectSummary.scss'
-import { FaPen } from 'react-icons/fa'
+/**@jsx jsx */
+import { jsx, css } from '@emotion/core'
+import { Fragment } from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
+import { FaPen } from 'react-icons/fa'
+
 import { deleteProject } from '../../../Store/Actions/ProjectsActions'
+import { colors, weights } from '../../../Styles'
 import PopUp from '../../PopUp/PopUp'
-import reactL from '../../../assets/logo/a144x144.png'
 
 const ProjectsSummary = ({ project, auth, ...props }) => {
+  const pWrapper = css`
+    border-bottom: 10px solid ${colors.darkBlue};
+    border-radius: 11%;
+    :hover,
+    :focus {
+      border-bottom-color: ${colors.aliceLightBlue};
+    }
+  `
+  const forHeader = css`
+    color: white;
+    background-color: ${colors.darkBlue};
+    padding: 8% 5%;
+    letter-spacing: 1.4px;
+    font-size: 1.82rem;
+    font-weight: ${weights.medium};
+    :hover,
+    :focus {
+      color: ${colors.independenceBlue};
+      background: ${colors.aliceLightBlue};
+    }
+  `
+
   return (
-    <div>
-      <div className='ProjectSummary'>
-        {auth.uid ? (
-          <div className='icons-svg'>
-            <NavLink to={`/edit/${project.id}`} key={project}>
-              <FaPen />
-            </NavLink>
-            <PopUp project={project} title={'Project'} />
-          </div>
-        ) : null}
-        {project.projectLogo[0] ? (
-          <img
-            alt="Project's logo"
-            src={
-              `https://images.weserv.nl/?url=${project.projectLogo[0]}` ||
-              reactL
-            }
-          />
-        ) : null}
-        <NavLink to={props.to} key={props.key}>
-          <h1 style={{ color: 'cornflowerblue' }}>{project.projectName}</h1>
-        </NavLink>
-      </div>
+    <div css={pWrapper}>
+      {auth.uid ? (
+        <Fragment>
+          <NavLink to={`/edit/${project.id}`} key={project}>
+            <FaPen />
+          </NavLink>
+          <PopUp project={project} title={'Project'} />
+        </Fragment>
+      ) : null}
+      <NavLink to={props.to} key={props.key}>
+        <h1 css={forHeader}>{project.projectName}</h1>
+      </NavLink>
+      <span
+        css={css`
+          padding: 10px 20px;
+          font-size: 108%;
+          color: ${colors.aliceLightBlue};
+        `}
+      >
+        Add Tags
+      </span>
     </div>
   )
 }
