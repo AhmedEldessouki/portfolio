5f37efa95457b831490543d6f43db49b6e2aa156
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-09-14 11:42:17 +0300

    removing all the envVars

--- src/Config/FirebaseConfig.js
+++ src/Config/FirebaseConfig.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 import 'firebase/storage'
 var config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: 'AIzaSyAABosRQdWuCOmy4ylwUjdi3Rlz3ngRcfM',
   authDomain: 'ahmedeldessouki-a7488.firebaseapp.com',
   databaseURL: 'https://ahmedeldessouki-a7488.firebaseio.com',
   projectId: 'ahmedeldessouki-a7488',
   storageBucket: 'gs://ahmedeldessouki-a7488.appspot.com',
   storage: 'gs://ahmedeldessouki-a7488.appspot.com',
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: '928636667018',
 }
 
 firebase.initializeApp(config)
