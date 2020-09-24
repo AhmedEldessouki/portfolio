69116bbf33dc5dc748a715774963030e1443ad0d
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-09-24 19:33:47 +0300

    some css edits for UnAuthNav

--- src/components/Navigation/UnAuthNavlinks.js
+++ src/components/Navigation/UnAuthNavlinks.js
@@ -11,18 +11,35 @@ const UnAuthNavlinks = () => {
     display: flex;
     justify-content: center;
   `
-
+  const h1a = css`
+    padding: 10px 23px;
+    color: ${colors.whiteFaded};
+    font-size: 2.6rem;
+    background: ${colors.independenceBlue};
+    margin: 16px 0;
+    letter-spacing: -0.5px;
+    border-radius: 12%;
+    :hover {
+      color: ${colors.aliceLightBlue};
+      background: ${colors.kindaDarkBlue};
+    }
+  `
+  const imgA = css`
+    width: 284px;
+    padding: 10px 15px;
+    background: ${colors.independenceBlue};
+    border-radius: 12%;
+    :hover {
+      background: ${colors.kindaDarkBlue};
+    }
+  `
   return (
     <div css={container}>
       <NavLink to="/">
         {my ? (
-          <img
-            src={my}
-            alt="Ahmed ElDessouki"
-            style={{width: '150px', padding: '0'}}
-          />
+          <img src={my} css={imgA} alt="Ahmed ElDessouki" />
         ) : (
-          <h1 style={{width: '150px', padding: '0'}}>Ahmed Eldessouki</h1>
+          <h1 css={h1a}>Ahmed Eldessouki</h1>
         )}
       </NavLink>
     </div>
