49ce5ed167025af1f8e58cad5cd3ebc000fabdb4
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-01-12 19:29:00 +0300

    signin & input to ts

--- src/components/Utils/Input.tsx
+++ src/components/Utils/Input.tsx
@@ -5,15 +5,21 @@ import {jsx} from '@emotion/react'
 import React from 'react'
 
 import {colors, labelWrapper, formWrapperInput} from '../Styles'
-
+interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
+  onChange?: () => void
+  onBlur?: (e: React.EventHandler<any> | any) => void
+  cleanColor: boolean
+  cssNew?: React.CSSProperties | any
+  name: string
+}
 function Input({
-  onChange: handleChange = () => {},
-  onBlur: handleBlur = () => {},
+  onChange: handleChange,
+  onBlur: handleBlur,
   cleanColor,
   cssNew,
   name,
   ...props
-}) {
+}: InputProps) {
   const [state, setState] = React.useState(colors.darkBlue)
 
   React.useEffect(() => {
@@ -34,7 +40,7 @@ function Input({
         onBlur={e => {
           const {valid} = e.target.validity
           valid ? setState(colors.lightGreen) : setState(colors.burgundyRed)
-          handleBlur(e)
+          handleBlur && handleBlur(e)
         }}
         {...props}
       />
