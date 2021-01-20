ce795bc65a0f44b0c11e3cbbc67e40cab76edeea
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-01-20 18:24:13 +0300

    Github Action Test Setup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.js': ['eslint', 'npm run test'],
+  '*.js': ['eslint', 'npm run test:precommit'],
   // '*.js': ['eslint'],
   '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
     'prettier --write',
