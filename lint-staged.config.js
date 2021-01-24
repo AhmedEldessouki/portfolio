9e22e4fc0358350d49a59c4325683d80f7acd156
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-01-24 14:30:10 +0300

    lint-staged cover ts files too

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  '*.js': ['eslint', 'npm run test:precommit'],
-  // '*.js': ['eslint', 'npm run test:precommit', 'npm run test:e2e'],
-  // '*.js': ['eslint'],
+  '*.+(js|jsx|ts|tsx)': ['eslint', 'npm run test:precommit'],
+  // '*.+(js|jsx|ts|tsx)': ['eslint', 'npm run test:precommit', 'npm run test:e2e'],
+  // '*.+(js|jsx|ts|tsx)': ['eslint'],
   '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
     'prettier --write',
   ],
