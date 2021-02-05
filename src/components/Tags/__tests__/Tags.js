29db16dedfdefede638f934ed4c0d51df5d3afee
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-02-05 16:56:04 +0300

    changes

--- src/components/Tags/__tests__/Tags.js
+++ src/components/Tags/__tests__/Tags.js
@@ -1,14 +1,15 @@
 import * as React from 'react'
-import {screen} from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
-import {render, userEvent} from '../../../test/app-test-utils'
-import {buildTag} from '../../../test/generate'
+import { render, userEvent } from '../../../test/app-test-utils'
+import { buildTag } from '../../../test/generate'
 import Tags from '../Tags'
+import { tags } from '../../../test/data/tags'
 
 const newTag = buildTag()
 
 test('Tag Form', async () => {
-  await render(<Tags tagsData={[buildTag(), buildTag()]} />, {
+  await render(<Tags tagsData={tags} />, {
     user: null,
     doWait: false,
   })
@@ -22,7 +23,7 @@ test('Tag Form', async () => {
   expect(screen.getByLabelText('name')).toHaveDisplayValue(newTag.name)
   expect(screen.getByLabelText(/url/i)).toHaveDisplayValue(newTag.url)
 
-  expect(screen.getByRole('button', {name: /create tag/i})).toHaveAttribute(
+  expect(screen.getByRole('button', { name: /create tag/i })).toHaveAttribute(
     'type',
     'submit',
   )
