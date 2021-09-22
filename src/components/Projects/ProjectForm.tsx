80e98b621dee7ee6293369309b8e3c8d8c531ea7
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-09-22 15:46:40 +0300

    fixing tags issue in writeProj

--- src/components/Projects/ProjectForm.tsx
+++ src/components/Projects/ProjectForm.tsx
@@ -1,7 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
-import {jsx} from '@emotion/react'
+import {css, jsx} from '@emotion/react'
 import React from 'react'
 import {colors, labelWrapper, formStyles, textArea, warning} from '../../Styles'
 import {
@@ -13,6 +13,30 @@ import {
 
 import type {ImportedImages, ReducerState} from '../../../types/types'
 import type {ProjectInterface} from '../../../types/interfaces'
+
+const selectStyling = css({
+  color: colors.whiteFaded,
+  background: colors.darkBlue,
+  margin: '2px 0',
+  height: 50,
+  letterSpacing: '2.2px',
+  width: '100%',
+  borderRadius: '11%',
+  padding: 8,
+  fontSize: '1.4rem',
+  border: `0 solid ${colors.independenceBlue}`,
+  boxShadow: `0 1px 0 1px ${colors.independenceBlue}`,
+  appearance: 'none',
+  backgroundImage: `url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+                linear-gradient(to bottom, ${colors.darkBlue} 0%,${colors.darkBlue} 100%)`,
+  backgroundRepeat: ` no-repeat, repeat`,
+  backgroundPosition: `right .7em top 50%, 0 0`,
+  backgroundSize: `.65em auto, 100%`,
+  option: {
+    borderRadius: '11%',
+    padding: 8,
+  },
+})
 
 function ProjectForm({
   useHandleSubmit,
@@ -35,6 +59,7 @@ function ProjectForm({
 }) {
   const {status, enteredProjectData, error} = state
   const {name, link, repoLink, projectType, tag} = enteredProjectData
+
   return (
     <form
       css={[formStyles, {gap: 6}]}
@@ -73,29 +98,7 @@ function ProjectForm({
         name="repoLink"
       />
       <select
-        css={{
-          color: colors.whiteFaded,
-          background: colors.darkBlue,
-          margin: '2px 0',
-          height: 50,
-          letterSpacing: '2.2px',
-          width: '100%',
-          borderRadius: '11%',
-          padding: 8,
-          fontSize: '1.4rem',
-          border: `0 solid ${colors.independenceBlue}`,
-          boxShadow: `0 1px 0 1px ${colors.independenceBlue}`,
-          appearance: 'none',
-          backgroundImage: `url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
-                        linear-gradient(to bottom, ${colors.darkBlue} 0%,${colors.darkBlue} 100%)`,
-          backgroundRepeat: ` no-repeat, repeat`,
-          backgroundPosition: `right .7em top 50%, 0 0`,
-          backgroundSize: `.65em auto, 100%`,
-          option: {
-            borderRadius: '11%',
-            padding: 8,
-          },
-        }}
+        css={selectStyling}
         defaultValue={projectType ?? 'personal'}
         name="projectType"
         aria-label="Please Select Project Type"
